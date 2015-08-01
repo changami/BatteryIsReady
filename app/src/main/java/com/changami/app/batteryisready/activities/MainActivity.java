ea0ada21577419c08b175faa3ff7fad86e404ffd
chan_gami <babyryo@msn.com>
2015-08-01 05:35:39 -0700

    :maple_leaf: implement to be able to choose percentage of battery

--- app/src/main/java/com/changami/app/batteryisready/activities/MainActivity.java
+++ app/src/main/java/com/changami/app/batteryisready/activities/MainActivity.java
@@ -7,18 +7,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 import com.changami.app.batteryisready.R;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    @InjectView(R.id.switch_on_off)
+    private final int MAX = 100;
+    private final int MIN = 0;
+
+    @InjectView(R.id.switchOnOff)
     Switch switchOnOff;
+    @InjectView(R.id.editValue)
+    TextView textView;
 
     SharedPreferences preference;
 
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         preference = getSharedPreferences(getString(R.string.preference_name), Context.MODE_PRIVATE);
         switchOnOff.setChecked(preference.getBoolean(getString(R.string.preference_available), false));
         switchOnOff.setOnCheckedChangeListener(this);
+        textView.setText(String.valueOf(preference.getInt(getString(R.string.preference_target_volume), MAX)));
     }
 
 
@@ -63,6 +73,61 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         SharedPreferences.Editor prefEditor = preference.edit();
         prefEditor.putBoolean(getString(R.string.preference_available), isChecked);
+        prefEditor.apply();
+    }
+
+    // TODO: 以下のクリックイベントをまとめる
+    @OnClick(R.id.plusButton)
+    void pushPlusButton(View v) {
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value < MAX) {
+            value++;
+        }
+        textView.setText(String.valueOf(value));
+        preserveTargetVolume(value);
+    }
+
+    @OnLongClick(R.id.plusButton)
+    boolean longPushPlusButton(View v) {
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value + 10 <= MAX) {
+            value += 10;
+        } else {
+            value = 100;
+        }
+        textView.setText(String.valueOf(value));
+        preserveTargetVolume(value);
+
+        return true;
+    }
+
+    @OnClick(R.id.minusButton)
+    void pushMinusButton(View v) {
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value > MIN) {
+            value--;
+        }
+        textView.setText(String.valueOf(value));
+        preserveTargetVolume(value);
+    }
+
+    @OnLongClick(R.id.minusButton)
+    boolean longPushMinusButton(View v) {
+        int value = Integer.parseInt(textView.getText().toString());
+        if (value - 10 >= MIN) {
+            value -= 10;
+        } else {
+            value = 0;
+        }
+        textView.setText(String.valueOf(value));
+        preserveTargetVolume(value);
+
+        return true;
+    }
+
+    private void preserveTargetVolume(int volume) {
+        SharedPreferences.Editor prefEditor = preference.edit();
+        prefEditor.putInt(getString(R.string.preference_target_volume), volume);
         prefEditor.apply();
     }
 }
--- app/src/main/java/com/changami/app/batteryisready/services/WatchingBatteryService.java
+++ app/src/main/java/com/changami/app/batteryisready/services/WatchingBatteryService.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
@@ -16,26 +17,31 @@ import com.changami.app.batteryisready.activities.MainActivity;
 
 public class WatchingBatteryService extends Service {
 
-    final int notificationId = 1;
+    final int NOTIFICATION_ON_SERVICE = 1;
+    final int NOTIFICATION_CANCEL = 2;
     final long[] PATTERN = {500, 2000};
 
     Vibrator vibrator;
     BroadcastReceiver broadcastReceiver;
     NotificationManagerCompat notificationManager;
+    SharedPreferences preference;
 
     @Override
     public void onCreate() {
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        preference = getSharedPreferences(getString(R.string.preference_name), MODE_PRIVATE);
 
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        final int targetVolume = preference.getInt(getString(R.string.preference_target_volume, ""), 100);
+
         // Intent to start MainActivity for clicking this app's notification
-        final Intent notificationIntent = new Intent(this,
+        Intent notificationIntent = new Intent(this,
                 MainActivity.class);
-        final PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, 0,
                 notificationIntent, 0);
 
         // Making Notification
@@ -43,11 +49,11 @@ public class WatchingBatteryService extends Service {
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(getString(R.string.app_name))
-                        .setContentText(getString(R.string.label_notification, 100))
-                        .setContentIntent(contentIntent);
+                        .setContentText(getString(R.string.notification_running, targetVolume))
+                        .setContentIntent(notificationPendingIntent);
         notificationManager =
                 NotificationManagerCompat.from(this);
-        notificationManager.notify(notificationId, notificationBuilder.build());
+        notificationManager.notify(NOTIFICATION_ON_SERVICE, notificationBuilder.build());
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
@@ -56,7 +62,17 @@ public class WatchingBatteryService extends Service {
             @Override
             public void onReceive(Context context, Intent intent) {
                 int batteryLevel = intent.getIntExtra(getString(R.string.battery_level), 0);
-                if (batteryLevel >= 100/*TODO*/) {
+                if (batteryLevel >= targetVolume) {
+                    // switch notification for stopping vibrator
+                    notificationManager.cancel(NOTIFICATION_ON_SERVICE);
+                    NotificationCompat.Builder cancelBuilder = new NotificationCompat.Builder(WatchingBatteryService.this)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle(getString(R.string.app_name))
+                            .setContentText(getString(R.string.notification_guide));
+                    notificationManager =
+                            NotificationManagerCompat.from(WatchingBatteryService.this);
+                    notificationManager.notify(NOTIFICATION_CANCEL, cancelBuilder.build());
+
                     vibrator.vibrate(PATTERN, 0);
                 }
             }
@@ -70,7 +86,8 @@ public class WatchingBatteryService extends Service {
     public void onDestroy() {
         unregisterReceiver(broadcastReceiver);
 
-        notificationManager.cancel(notificationId);
+        notificationManager.cancel(NOTIFICATION_ON_SERVICE);
+        notificationManager.cancel(NOTIFICATION_CANCEL);
         vibrator.cancel();
 
         super.onDestroy();
